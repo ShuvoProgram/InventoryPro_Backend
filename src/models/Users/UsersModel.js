c95a597af588035bad2b3edb033bbc8aca4c5990
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 16:03:48 +0600

    Add user schema in mongoose model

---
+++ src/models/Users/UsersModel.js
@@ -0,0 +1,16 @@
+let mongoose = require("mongoose");
+let userSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String, unique: true },
+    firstName: { type: String },
+    lastName: { type: String },
+    mobile: { type: String },
+    password: { type: String },
+    photo: { type: String },
+    createdDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+let UserModel = mongoose.model("users", userSchema);
+module.exports = UserModel;
