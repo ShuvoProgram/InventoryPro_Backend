03572b59cfeb0c84430f205a996ca4015f68d3d6
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 16:21:34 +0600

    Add CreateToken function using jwt

---
+++ src/utility/CreateToken.js
@@ -0,0 +1,9 @@
+const jwt = require("jsonwebtoken");
+
+const CreateToken = async (data) => {
+  let Payload = {exp: Math.floor(Date.now() / 1000) + (24*60 * 60),data: data};
+
+  return await jwt.sign(Payload, "jg45rt7p87ksd4oap5qv5of");
+};
+
+module.exports = CreateToken;
