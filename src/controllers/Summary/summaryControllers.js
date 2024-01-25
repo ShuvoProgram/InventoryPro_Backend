7a2a47c30e2a278cd62cfddd0a64fa7eaca6ef5f
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 17:55:34 +0600

    Add summary services and controllers

---
+++ src/controllers/Summary/summaryControllers.js
@@ -0,0 +1,28 @@
+const ExpenseSummaryService = require("../../services/Summary/ExpenseSummaryService");
+const PurchaseSummaryService = require("../../services/Summary/PurchaseSummaryService");
+const ReturnSummaryService = require("../../services/Summary/ReturnSummaryService");
+const SalesSummaryService = require("../../services/Summary/SalesSummaryService");
+
+// ExpensesSummary
+exports.ExpensesSummary=async (req, res) => {
+    let Result=await ExpenseSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// ReturnSummary
+exports.ReturnSummary=async (req, res) => {
+    let Result=await ReturnSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// PurchaseSummary
+exports.PurchaseSummary=async (req, res) => {
+    let Result=await PurchaseSummaryService(req)
+    res.status(200).json(Result);
+}
+
+// SalesSummary
+exports.SalesSummary=async (req, res) => {
+    let Result=await SalesSummaryService(req)
+    res.status(200).json(Result);
+}
