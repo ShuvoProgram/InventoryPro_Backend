79fc0913964e1cd812481ff9616d768685d92530
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 18:13:47 +0600

    Add ReturnSummaryService function

---
+++ src/services/Summary/ReturnSummaryService.js
@@ -0,0 +1,37 @@
+const ReturnsModel = require("../../models/Return/ReturnsModel");
+
+const ReturnSummaryService = async (req) => {
+  try {
+    let UserEmail = req.headers["email"];
+
+    let data = await ReturnsModel.aggregate([
+      {
+        $match: {
+          UserEmail: UserEmail,
+        },
+      },
+      {
+        $facet: {
+          Total: [{ $group: { _id: 0, TotalAmount: { $sum: "$GrandTotal" } } }],
+          Last30Days: [
+            {
+              $group: {
+                _id: {
+                  $dateToString: { format: "%Y-%m-%d", date: "$CreatedDate" },
+                },
+                TotalAmount: { $sum: "$GrandTotal" },
+              },
+            },
+            { $sort: { _id: -1 } },
+            { $limit: 30 },
+          ],
+        },
+      },
+    ]);
+    return { status: "success", data: data };
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = ReturnSummaryService;
