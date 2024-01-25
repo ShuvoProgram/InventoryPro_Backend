c435c980d3fdba25ab9fdb701af6d483dd729aad
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 16:12:53 +0600

    Add purchase products schema

---
+++ src/models/Purchases/PurchaseProductsModel.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const purchaseProductsSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    PurchaseID: { type: ObjectId },
+    ProductID: { type: ObjectId },
+    Qty: { type: Number },
+    UnitCost: { type: Number },
+    Total: { type: Number },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const PurchaseProductsModel = mongoose.model("purchaseproducts",purchaseProductsSchema
+);
+
+module.exports = PurchaseProductsModel;
