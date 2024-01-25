23740a46061ab6469dbeb7f1e6589fb16e774578
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 18:20:27 +0600

    Implement UserVerifyOtpService 🔐

---
+++ src/services/user/UserVerifyOtpService.js
@@ -0,0 +1,30 @@
+const UserVerifyOtpService = async (request, dataModel) => {
+  try {
+    // parameter catch
+    let email = request.params.email;
+    let otpCode = request.params.otp;
+    let status = 0;
+    let statusUpdate = 1;
+
+    let otpCount = await dataModel.aggregate([
+      { $match: {UserEmail:email, otp: otpCode, status } },
+      { $count: "total" },
+    ]);
+
+    if (otpCount.length > 0) {
+      //otp update
+      let OTPUpdate = await dataModel.updateOne(
+        { UserEmail:email, otp: otpCode, status },
+        { status: statusUpdate }
+      );
+
+      return { status: "success", data: OTPUpdate };
+    } else {
+      return { status: "fail", data: "Invalid OTP Code!" };
+    }
+  } catch (error) {
+    return { status: "fail", error: error.message };
+  }
+};
+
+module.exports = UserVerifyOtpService;
