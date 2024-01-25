eb859e1fe693f26544d7769ece01f18141d95487
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 16:17:45 +0600

    Add SaleProductsModel schema

---
+++ src/models/Sales/SalesProductsModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const saleProductsSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    SaleID: { type: ObjectId },
+    ProductID: { type: ObjectId },
+    Qty: { type: Number },
+    UnitCost: { type: Number },
+    Total: { type: Number },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const SaleProductsModel = mongoose.model("saleproducts",saleProductsSchema);
+
+module.exports = SaleProductsModel;
