a340b7557ecf151c0bb2e8562208d9308ed4341c
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 16:16:03 +0600

    Add ReturnProductsModel schema

---
+++ src/models/Return/ReturnProductsModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema.Types;
+
+const returnProductsSchema = new mongoose.Schema(
+  {
+    UserEmail: { type: String },
+    ReturnID: { type: ObjectId },
+    ProductID: { type: ObjectId },
+    Qty: { type: Number },
+    UnitCost: { type: Number },
+    Total: { type: Number },
+    CreatedDate: { type: Date, default: Date.now() },
+  },
+  { versionKey: false }
+);
+
+const ReturnProductsModel = mongoose.model("returnproducts",returnProductsSchema);
+
+module.exports = ReturnProductsModel;
