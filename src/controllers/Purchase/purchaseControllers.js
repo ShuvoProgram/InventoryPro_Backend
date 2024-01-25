e841cae2b0d221a84988a4c31fa4ce501057ff0e
ShuvoProgram <shuvoprogramer@gmail.com>
2024-01-25 17:46:46 +0600

    🚧 Database import and CRUD operations for Purchases

---
+++ src/controllers/Purchase/purchaseControllers.js
@@ -0,0 +1,55 @@
+//data base import
+const ParentModel = require("../../models/Purchases/PurchasesModel");
+const ChildeModel = require("../../models/Purchases/PurchaseProductsModel");
+const ParentChildeService = require("../../services/common/createParentChildsService");
+const ListOneJoinService = require("../../services/common/listOneJoinService");
+const DeleteParentChildeService = require("../../services/common/deleteParentChildsService");
+
+// Create Purchases
+exports.CreatePurchases = async (req, res) => {
+  let Result = await ParentChildeService(
+    req,
+    ParentModel,
+    ChildeModel,
+    "PurchaseID"
+  );
+  res.status(200).json(Result);
+};
+
+// Purchases List
+exports.PurchasesList = async (req, res) => {
+  let SearchRgx = { $regex: req.params.searchKeyword, $options: "i" };
+  let SearchArray = [
+    { Note: SearchRgx },
+    { "suppliers.Name": SearchRgx },
+    { "suppliers.Address": SearchRgx },
+    { "suppliers.Phone": SearchRgx },
+    { "suppliers.Email": SearchRgx },
+  ];
+  let JoinStage = {
+    $lookup: {
+      from: "suppliers",
+      localField: "SupplierID",
+      foreignField: "_id",
+      as: "suppliers",
+    },
+  };
+  let result = await ListOneJoinService(
+    req,
+    ParentModel,
+    SearchArray,
+    JoinStage
+  );
+  res.status(200).json(result);
+};
+
+// Delete Purchases
+exports.DeletePurchases = async (req, res) => {
+  let Result = await DeleteParentChildeService(
+    req,
+    ParentModel,
+    ChildeModel,
+    "PurchaseID"
+  );
+  res.status(200).json(Result);
+};
